6963d0b6b12666275b493627a12e4f6a1a025939
tetiana-ukr <122030789+tetiana-ukr@users.noreply.github.com>
2023-06-12 17:23:58 +0000

    Deploying to gh-pages from @ tetiana-ukr/goit-js-hw-09@acfce7da44a0e7de80ac4cbb006ca764142d1728 🚀

---
+++ 01-color-switcher.a8f53a1b.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),n=document.querySelector("body"),o=null;function r(){n.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0))}t.addEventListener("click",(function(){o=setInterval(r,1e3),t.setAttribute("disabled","true")})),e.addEventListener("click",(function(){clearInterval(o),t.removeAttribute("disabled")}))}();
+//# sourceMappingURL=01-color-switcher.a8f53a1b.js.map
---
+++ 01-color-switcher.f143b892.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),n=document.querySelector("body");let o=null;function r(){n.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}t.addEventListener("click",(function(){o=setInterval(r,1e3),t.setAttribute("disabled","true")})),e.addEventListener("click",(function(){clearInterval(o),t.removeAttribute("disabled")}));
+//# sourceMappingURL=01-color-switcher.f143b892.js.map
